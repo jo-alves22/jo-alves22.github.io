d92eeeab4e96cf50d145bf30b86fd9a2cbea559b
jo-alves22 <jonasadrianoalves@gmail.com>
2022-07-20 07:39:21 -0300

    Adicionando todas as paginas

---
+++ js/main.js
@@ -0,0 +1,51 @@
+
+function mudacormenu(){
+    var cormenu = document.getElementById("dropdownMenuLink");
+    cormenu.style.color="rgb(0, 128, 128)";            
+}
+function voltacormenu(){
+    var cor = document.getElementById("dropdownMenuLink");
+    cor.style.color="white";   
+}
+function mudacorclapale(){
+    var coritem = document.getElementById("dropdown-clapale");
+    coritem.style.color="rgb(0, 128, 128)";
+}
+function voltacorclapale(){
+    var coritem = document.getElementById("dropdown-clapale");
+    coritem.style.color="white";
+}
+function mudacorhotel(){
+    var coritem = document.getElementById("dropdown-hotel");
+    coritem.style.color="rgb(0, 128, 128)";
+}
+function voltacorhotel(){
+    var coritem = document.getElementById("dropdown-hotel");
+    coritem.style.color="white";
+}
+function mudacorvoz(){
+    var coritem = document.getElementById("dropdown-voz");
+    coritem.style.color="rgb(0, 128, 128)";
+}
+function voltacorvoz(){
+    var coritem = document.getElementById("dropdown-voz");
+    coritem.style.color="white";
+}
+const scrollAnima = document.querySelector('[data-anima="scroll"]');
+
+const metadeWindow = window.innerHeight * 2.0;
+console.log(metadeWindow);
+function animarScroll(){
+    const topoItem = scrollAnima.getBoundingClientRect().top;
+
+    console.log(topoItem);
+
+    const itemVisivel = topoItem - metadeWindow < 0;
+    if(itemVisivel){
+        scrollAnima.classList.add('show-button');
+    }else{
+        scrollAnima.classList.remove('show-button');
+    };
+}
+
+window.addEventListener('scroll',animarScroll);
